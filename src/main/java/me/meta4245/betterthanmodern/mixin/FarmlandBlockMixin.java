910107846f0ee98feb3d3fe5d2b900832e1c9f0a
Metaa4245 <cappymeta@gmail.com>
2024-08-27 23:24:38 +0200

    remove farmland trample by stepping

---
+++ src/main/java/me/meta4245/betterthanmodern/mixin/FarmlandBlockMixin.java
@@ -0,0 +1,24 @@
+package me.meta4245.betterthanmodern.mixin;
+
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FarmlandBlock.class)
+public abstract class FarmlandBlockMixin {
+    @Inject(method = "onSteppedOn", at = @At("HEAD"), cancellable = true)
+    public void onSteppedOn(
+            World world,
+            int x,
+            int y,
+            int z,
+            Entity entity,
+            CallbackInfo ci)
+    {
+        ci.cancel();
+    }
+}
