69ae26681878ca94c744d07b1377dbcea38253cb
Metaa4245 <cappymeta@gmail.com>
2024-08-28 23:09:48 +0200

    organize imports

--- src/main/java/me/meta4245/betterthanmodern/event/BlockRegistry.java
+++ src/main/java/me/meta4245/betterthanmodern/event/BlockRegistry.java
@@ -1,7 +1,9 @@
 package me.meta4245.betterthanmodern.event;
 
 import me.meta4245.betterthanmodern.ReflectionHacks;
-import me.meta4245.betterthanmodern.block.*;
+import me.meta4245.betterthanmodern.block.CoalBlock;
+import me.meta4245.betterthanmodern.block.RedstoneBlock;
+import me.meta4245.betterthanmodern.block.SmoothStone;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.minecraft.block.Block;
 import net.modificationstation.stationapi.api.event.registry.BlockRegistryEvent;
