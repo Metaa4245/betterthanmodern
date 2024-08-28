9286c3eb5b1e08fe84c9a78ea03023fcd873b252
Metaa4245 <cappymeta@gmail.com>
2024-08-28 18:43:14 +0200

    make coal block usable in furnaces

---
+++ src/main/java/me/meta4245/betterthanmodern/mixin/FurnaceBlockEntityMixin.java
@@ -0,0 +1,19 @@
+package me.meta4245.betterthanmodern.mixin;
+
+import me.meta4245.betterthanmodern.event.BlockRegistry;
+import net.minecraft.block.entity.FurnaceBlockEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FurnaceBlockEntity.class)
+public abstract class FurnaceBlockEntityMixin {
+    @Inject(method = "getFuelTime", at = @At("TAIL"), cancellable = true)
+    public void getFuelTime(ItemStack i, CallbackInfoReturnable<Integer> cir) {
+        if (i.getItem().id == BlockRegistry.coalBlock.id) {
+            cir.setReturnValue(16000);
+        }
+    }
+}
