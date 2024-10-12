122ef4ffd3c802a6051ff02ddedd0f0aa528cf64
Metaa4245 <cappymeta@gmail.com>
2024-10-12 19:14:50 +0200

    remove broken features, bump to 1.2.0

--- src/main/java/me/meta4245/betterthanmodern/block/RedstoneBlock.java
+++ src/main/java/me/meta4245/betterthanmodern/block/RedstoneBlock.java
@@ -1,8 +1,6 @@
 package me.meta4245.betterthanmodern.block;
 
 import net.minecraft.block.material.Material;
-import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.template.block.TemplateBlock;
 import net.modificationstation.stationapi.api.util.Identifier;
 
@@ -11,52 +9,5 @@ public class RedstoneBlock extends TemplateBlock {
         super(identifier, Material.METAL);
         super.setHardness(5);
         super.setResistance(6);
-    }
-
-    @Override
-    public int getTickRate() {
-        return 2;
-    }
-
-    @Override
-    public boolean canEmitRedstonePower() {
-        return true;
-    }
-
-    @Override
-    public boolean isEmittingRedstonePowerInDirection(BlockView blockView, int x, int y, int z, int direction) {
-        return true;
-    }
-
-    @Override
-    public boolean canTransferPowerInDirection(World world, int x, int y, int z, int direction) {
-        return true;
-    }
-
-    @Override
-    public void onPlaced(World world, int x, int y, int z) {
-        world.notifyNeighbors(x, y - 1, z, this.id);
-        world.notifyNeighbors(x, y + 1, z, this.id);
-        world.notifyNeighbors(x - 1, y, z, this.id);
-        world.notifyNeighbors(x + 1, y, z, this.id);
-        world.notifyNeighbors(x, y, z - 1, this.id);
-        world.notifyNeighbors(x, y, z + 1, this.id);
-    }
-
-    @Override
-    public void onBreak(World world, int x, int y, int z) {
-        world.notifyNeighbors(x, y - 1, z, this.id);
-        world.notifyNeighbors(x, y + 1, z, this.id);
-        world.notifyNeighbors(x - 1, y, z, this.id);
-        world.notifyNeighbors(x + 1, y, z, this.id);
-        world.notifyNeighbors(x, y, z - 1, this.id);
-        world.notifyNeighbors(x, y, z + 1, this.id);
-    }
-
-    @Override
-    public void neighborUpdate(World world, int x, int y, int z, int id) {
-        super.neighborUpdate(world, x, y, z, id);
-
-        world.scheduleBlockUpdate(x, y, z, this.id, this.getTickRate());
     }
 }
